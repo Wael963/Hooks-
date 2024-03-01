8acdd9d88a85b527aa7c0a9c5cd00d560ea2451c
wael963 <waelbouraoui0@gmail.com>
2024-03-01 10:45:23 +0100

    Hooks

---
+++ src/components/Moviecard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import StarRatingComponent from 'react-star-rating-component';
+
+
+const Moviecard = (props) => {
+  return (
+    <div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={props.movie.posterURL} />
+        <Card.Body>
+          <Card.Title>{props.movie.title}</Card.Title>
+          <Card.Text>{props.movie.description}</Card.Text>
+          
+          <StarRatingComponent
+            name="rate1"
+            starCount={10}
+            value={props.movie.rating}
+
+          />
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
+
+export default Moviecard
